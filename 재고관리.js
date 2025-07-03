6985e29474961a2960745e140a9a6caa13089d27
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-03 20:13:20 +0900

    재고관리 재고추가 수정

--- 재고관리.js
+++ 재고관리.js
@@ -22,39 +22,80 @@ function stockPrint() {
     }
     body.innerHTML = html;
 }
-function s_retouchBtn(stockcode){
-    console.log('s_retouchBtn');
-    let category = prompt("카테고리를 입력하세요.");
-    let productName = prompt("상품명을 입력하세요.");
-    let stockIn = prompt("입고수량을 입력하세요.");
-    let stockUsed = prompt("입고된 뒤 사용량을 입력하세요.");
-    let minStockLevel = prompt("최소재고기준을 입력하세요.");
-    let notes = prompt("메모사항을 입력하세요.");
-    let stockList = getStockList();
-    for ( let i = 0; i <= stockList.length - 1; i++){
-        let stock = stockList[i];
-        if ( stock.productId == stockcode ){
-            console.log(stock);
-            stock.category = category;
-            stock.productName = productName;
-            stock.stockIn = stockIn;
-            stock.stockUsed = stockUsed;
-            stock.minStockLevel = minStockLevel;
-            stock.notes = notes;
-            setStockList(stockList);
-            stockPrint();
-        }
-    }
-}
+// function s_retouchBtn(stockcode){
+//     console.log('s_retouchBtn');
+//     let category = prompt("카테고리를 입력하세요.");
+//     let productName = prompt("상품명을 입력하세요.");
+//     let stockIn = prompt("입고수량을 입력하세요.");
+//     let stockUsed = prompt("입고된 뒤 사용량을 입력하세요.");
+//     let minStockLevel = prompt("최소재고기준을 입력하세요.");
+//     let notes = prompt("메모사항을 입력하세요.");
+//     let stockList = getStockList();
+//     for ( let i = 0; i <= stockList.length - 1; i++){
+//         let stock = stockList[i];
+//         if ( stock.productId == stockcode ){
+//             console.log(stock);
+//             stock.category = category;
+//             stock.productName = productName;
+//             stock.stockIn = stockIn;
+//             stock.stockUsed = stockUsed;
+//             stock.minStockLevel = minStockLevel;
+//             stock.notes = notes;
+//             setStockList(stockList);
+//             stockPrint();
+//         }
+//     }
+// }
 
+// function s_plus(){
+//     console.log('s_plus');
+//     let category = prompt("카테고리를 입력하세요.");
+//     let productName = prompt("상품명을 입력하세요.");
+//     let stockIn = prompt("입고수량을 입력하세요.");
+//     let stockUsed = prompt("입고된 뒤 사용량을 입력하세요.");
+//     let minStockLevel = prompt("최소재고기준을 입력하세요.");
+//     let notes = prompt("메모사항을 입력하세요.");
+//     let productId = stockList.length == 0 ? 1 : stockList[stockList.length-1].productId + 1;
+//     let obj = {
+//         productId,
+//         category,
+//         productName,
+//         stockIn,
+//         stockUsed,
+//         minStockLevel,
+//         notes
+//     }
+//     stockList.push( obj );
+//     setStockList(stockList);
+//     stockPrint();
+// }
 function s_plus(){
-    console.log('s_plus');
-    let category = prompt("카테고리를 입력하세요.");
-    let productName = prompt("상품명을 입력하세요.");
-    let stockIn = prompt("입고수량을 입력하세요.");
-    let stockUsed = prompt("입고된 뒤 사용량을 입력하세요.");
-    let minStockLevel = prompt("최소재고기준을 입력하세요.");
-    let notes = prompt("메모사항을 입력하세요.");
+    console.log('1111');
+    let stockList = getStockList();
+    let body = document.querySelector('.m_tbody');
+    let html = ``
+    let num = stockList[stockList.length-1].productId + 1;
+    html += `<tr>
+                <td>${num}</td>
+                <td><input class="cate_input" type="text" placeholder="카테고리"></td>
+                <td><input class="name_input" type="text" placeholder="상품명"></td>
+                <td><input class="inst_input" type="text" placeholder="입고수량"></td>
+                <td><input class="usst_input" type="text" placeholder="사용량"></td>
+                <td><input class="mins_input" type="text" placeholder="최소재고기준"></td>
+                <td><input class="note_input" type="text" placeholder="유의사항"></td>
+                <td>
+                    <button onclick="spBtn()">추가완료</button>
+                </td>
+                </tr>`
+    body.innerHTML += html;
+};
+function spBtn(){
+    let category      = document.querySelector('.cate_input').value;
+    let productName   = document.querySelector('.name_input').value;
+    let stockIn       = document.querySelector('.inst_input').value;
+    let stockUsed     = document.querySelector('.usst_input').value;
+    let minStockLevel = document.querySelector('.mins_input').value;
+    let notes         = document.querySelector('.note_input').value;
     let productId = stockList.length == 0 ? 1 : stockList[stockList.length-1].productId + 1;
     let obj = {
         productId,
