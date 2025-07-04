f253ff293351487f6a6fd424d0dcb0899673cd28
JeonghoonAHN0510 <Jeonghoonahn0510@gmail.com>
2025-07-04 11:58:37 +0900

    11시 58분

--- 박옥선.js
+++ 박옥선.js
@@ -1,55 +1,56 @@
-let userList = [
-  { userCode: 1, userName: '유재석', userNum: '01011112222', userEmail: 'qwe123@qwe123.com', userPwd: 'qwe123' },
-  { userCode: 2, userName: '김종국', userNum: '01012345678', userEmail: 'kimjk12@example.com', userPwd: 'pass1234' },
-  { userCode: 3, userName: '박명수', userNum: '01087654321', userEmail: 'parkms@example.com', userPwd: 'abc12345' },
-  { userCode: 4, userName: '정형돈', userNum: '01022223333', userEmail: 'donhyung@example.com', userPwd: 'donny456' },
-  { userCode: 5, userName: '하하', userNum: '01033334444', userEmail: 'haha@example.com', userPwd: 'haha5678' },
-  { userCode: 6, userName: '송지효', userNum: '01044445555', userEmail: 'jihyo.song@example.com', userPwd: 'songpw99' },
-  { userCode: 7, userName: '이광수', userNum: '01055556666', userEmail: 'kwangsoo@example.com', userPwd: 'kwang22' },
-  { userCode: 8, userName: '전소민', userNum: '01066667777', userEmail: 'somin.jeon@example.com', userPwd: 'somin456' },
-  { userCode: 9, userName: '양세찬', userNum: '01077778888', userEmail: 'yangsc@example.com', userPwd: 'yangpw78' },
-  { userCode: 10, userName: '이효리', userNum: '01088889999', userEmail: 'leehl@example.com', userPwd: 'hyolee01' },
-  { userCode: 11, userName: '비', userNum: '01099990000', userEmail: 'rain@rain.com', userPwd: 'rain123' },
-  { userCode: 12, userName: '장도연', userNum: '01011110000', userEmail: 'jangdy@example.com', userPwd: 'doyeon12' },
-  { userCode: 13, userName: '김숙', userNum: '01022221111', userEmail: 'kimsook@example.com', userPwd: 'sookpass' },
-  { userCode: 14, userName: '홍진경', userNum: '01033332222', userEmail: 'hongjk@example.com', userPwd: 'hongpw88' },
-  { userCode: 15, userName: '조세호', userNum: '01044443333', userEmail: 'joeho@example.com', userPwd: 'seho789' },
-  { userCode: 16, userName: '강호동', userNum: '01055554444', userEmail: 'kanghd@example.com', userPwd: 'hodong99' },
-  { userCode: 17, userName: '이수근', userNum: '01066665555', userEmail: 'leesg@example.com', userPwd: 'soogeun1' },
-  { userCode: 18, userName: '서장훈', userNum: '01077776666', userEmail: 'seojh@example.com', userPwd: 'janghoon9' },
-  { userCode: 19, userName: '김연경', userNum: '01088887777', userEmail: 'yeonkk@example.com', userPwd: 'volley22' },
-  { userCode: 20, userName: '기안84', userNum: '01099998888', userEmail: 'kian84@webtoon.com', userPwd: 'kian8484' },
-  { userCode: 21, userName: '이영지', userNum: '01012121212', userEmail: 'youngji@example.com', userPwd: 'ygj222' },
-  { userCode: 22, userName: '유아인', userNum: '01034343434', userEmail: 'yooin@example.com', userPwd: 'yooin99' },
-  { userCode: 23, userName: '정은지', userNum: '01056565656', userEmail: 'eunji@example.com', userPwd: 'pink987' },
-  { userCode: 24, userName: '이찬원', userNum: '01078787878', userEmail: 'chanwon@example.com', userPwd: 'chan123' },
-  { userCode: 25, userName: '임영웅', userNum: '01090909090', userEmail: 'youngwoong@example.com', userPwd: 'hero8888' },
-  { userCode: 26, userName: '김범수', userNum: '01001010101', userEmail: 'bsk@example.com', userPwd: 'voice99' },
-  { userCode: 27, userName: '백현', userNum: '01002020202', userEmail: 'baekhyun@exo.com', userPwd: 'exo4444' },
-  { userCode: 28, userName: '아이유', userNum: '01003030303', userEmail: 'iu@dlwlrma.com', userPwd: 'iu2020' },
-  { userCode: 29, userName: '태연', userNum: '01004040404', userEmail: 'taeyeon@gg.com', userPwd: 'snsd123' },
-  { userCode: 30, userName: '제시', userNum: '01005050505', userEmail: 'jessie@example.com', userPwd: 'strongpw' }
-];
+// let userList = [
+//   { userCode: 1, userName: '유재석', userNum: '01011112222', userEmail: 'qwe123@qwe123.com', userPwd: 'qwe123' },
+//   { userCode: 2, userName: '김종국', userNum: '01012345678', userEmail: 'kimjk12@example.com', userPwd: 'pass1234' },
+//   { userCode: 3, userName: '박명수', userNum: '01087654321', userEmail: 'parkms@example.com', userPwd: 'abc12345' },
+//   { userCode: 4, userName: '정형돈', userNum: '01022223333', userEmail: 'donhyung@example.com', userPwd: 'donny456' },
+//   { userCode: 5, userName: '하하', userNum: '01033334444', userEmail: 'haha@example.com', userPwd: 'haha5678' },
+//   { userCode: 6, userName: '송지효', userNum: '01044445555', userEmail: 'jihyo.song@example.com', userPwd: 'songpw99' },
+//   { userCode: 7, userName: '이광수', userNum: '01055556666', userEmail: 'kwangsoo@example.com', userPwd: 'kwang22' },
+//   { userCode: 8, userName: '전소민', userNum: '01066667777', userEmail: 'somin.jeon@example.com', userPwd: 'somin456' },
+//   { userCode: 9, userName: '양세찬', userNum: '01077778888', userEmail: 'yangsc@example.com', userPwd: 'yangpw78' },
+//   { userCode: 10, userName: '이효리', userNum: '01088889999', userEmail: 'leehl@example.com', userPwd: 'hyolee01' },
+//   { userCode: 11, userName: '비', userNum: '01099990000', userEmail: 'rain@rain.com', userPwd: 'rain123' },
+//   { userCode: 12, userName: '장도연', userNum: '01011110000', userEmail: 'jangdy@example.com', userPwd: 'doyeon12' },
+//   { userCode: 13, userName: '김숙', userNum: '01022221111', userEmail: 'kimsook@example.com', userPwd: 'sookpass' },
+//   { userCode: 14, userName: '홍진경', userNum: '01033332222', userEmail: 'hongjk@example.com', userPwd: 'hongpw88' },
+//   { userCode: 15, userName: '조세호', userNum: '01044443333', userEmail: 'joeho@example.com', userPwd: 'seho789' },
+//   { userCode: 16, userName: '강호동', userNum: '01055554444', userEmail: 'kanghd@example.com', userPwd: 'hodong99' },
+//   { userCode: 17, userName: '이수근', userNum: '01066665555', userEmail: 'leesg@example.com', userPwd: 'soogeun1' },
+//   { userCode: 18, userName: '서장훈', userNum: '01077776666', userEmail: 'seojh@example.com', userPwd: 'janghoon9' },
+//   { userCode: 19, userName: '김연경', userNum: '01088887777', userEmail: 'yeonkk@example.com', userPwd: 'volley22' },
+//   { userCode: 20, userName: '기안84', userNum: '01099998888', userEmail: 'kian84@webtoon.com', userPwd: 'kian8484' },
+//   { userCode: 21, userName: '이영지', userNum: '01012121212', userEmail: 'youngji@example.com', userPwd: 'ygj222' },
+//   { userCode: 22, userName: '유아인', userNum: '01034343434', userEmail: 'yooin@example.com', userPwd: 'yooin99' },
+//   { userCode: 23, userName: '정은지', userNum: '01056565656', userEmail: 'eunji@example.com', userPwd: 'pink987' },
+//   { userCode: 24, userName: '이찬원', userNum: '01078787878', userEmail: 'chanwon@example.com', userPwd: 'chan123' },
+//   { userCode: 25, userName: '임영웅', userNum: '01090909090', userEmail: 'youngwoong@example.com', userPwd: 'hero8888' },
+//   { userCode: 26, userName: '김범수', userNum: '01001010101', userEmail: 'bsk@example.com', userPwd: 'voice99' },
+//   { userCode: 27, userName: '백현', userNum: '01002020202', userEmail: 'baekhyun@exo.com', userPwd: 'exo4444' },
+//   { userCode: 28, userName: '아이유', userNum: '01003030303', userEmail: 'iu@dlwlrma.com', userPwd: 'iu2020' },
+//   { userCode: 29, userName: '태연', userNum: '01004040404', userEmail: 'taeyeon@gg.com', userPwd: 'snsd123' },
+//   { userCode: 30, userName: '제시', userNum: '01005050505', userEmail: 'jessie@example.com', userPwd: 'strongpw' }
+// ];
 
 // ===== 1. localStorage 관리하는 함수 ======
 function setLocalStorage(userList) {
   localStorage.setItem('userList', JSON.stringify(userList));//문자열만 저장할수있기 때문에 JSON문자열로 변환
   console.log(userList);
 }
-
+// userList = setLocalStorage(userList);
 // ===== 2. 회원가입 함수 =====
 function signupBotton() {
   console.log('--> signupBotton exe')
-  let userCode = 1 // 회원번호 초기값
+  // 회원번호 초기값
+
   // ===== localStorage에서 userList 가져오기 =====
   // 1) localStorage에서 userList 가져오기
   let userList = localStorage.getItem('userList');
+
   // 2) 존재하지 않으면 (배열) 새로 생성, 존재하면 타입변환
   if (userList == null) {
     userList = [];
   } else {
     userList = JSON.parse(userList);
-    userCode = userList[userList.length - 1].userCode + 1;
   }
   // (1) 입력 마크업 객체가져오기 
   const signup_name = document.querySelector('#signup_name'); console.log(signup_name);
@@ -57,7 +58,7 @@ function signupBotton() {
   const signup_email = document.querySelector('#signup_email');
   const signup_pwd = document.querySelector('#signup_pwd');
   const confirmPassword = document.querySelector('#confirmPassword');
-
+  let userCode = userList.length == 0 ? 1 : userList[userList.length - 1].userCode + 1;
   // (2) 입력마크업 객체내 입력값 가져오기
   const userName = signup_name.value; console.log(userName);
   const userNum = signup_phone.value; console.log(userNum);
@@ -90,13 +91,12 @@ function signupBotton() {
       alert(" 중복된 이메일입니다. 다른 이메일을 입력하세요. ")
       return;
     }
+    if (userList[i].userNum == userNum) {
+      alert(" 중복된 전화번호입니다. 다른 전화번호를 입력하세요. ")
+      return;
+    }
   }
-}
-// 전화번호 중복 확인
-for (let i = 0; i <= userList.length - 1; i++) {
-  //     if(userList[i].userEmail == userEmail){
-  //       alert(" 중복된 이메일입니다. 다른 이메일을 입력하세요. ")
-  //       return;
+  // 전화번호 중복 확인
 
 
 
@@ -123,29 +123,31 @@ for (let i = 0; i <= userList.length - 1; i++) {
   confirmPassword.value = '';
   alert('회원 가입 성공');
   location.href = '정은주.html'
+
 }
 
 
-function pwdPrint(){
+
+function pwdPrint() {
   let pwd = document.querySelector('#signup_pwd');
   let img = document.querySelector('.imgpwd');
-  if( pwd.type =='text'){
+  if (pwd.type == 'text') {
     pwd.type = "password";
     img.src = "KakaoTalk_20250702_172831311.png";
-  }else{ 
-    pwd.type ="text"
+  } else {
+    pwd.type = "text"
     img.src = "KakaoTalk_20250702_173318028.png";
   }
 }
 
-function pwdPrint1(){
+function pwdPrint1() {
   let pwd1 = document.querySelector('#confirmPassword');
   let img1 = document.querySelector('.imgpwd1');
-  if( pwd1.type =='text'){
+  if (pwd1.type == 'text') {
     pwd1.type = "password";
     img1.src = "KakaoTalk_20250702_172831311.png";
-  }else{ 
-    pwd1.type ="text"
+  } else {
+    pwd1.type = "text"
     img1.src = "KakaoTalk_20250702_173318028.png";
   }
 }
